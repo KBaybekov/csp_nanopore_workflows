e6d1467f2f6050e4e008f8b1beec04d0467723c1
KBaybekov <91565993+KBaybekov@users.noreply.github.com>
2024-12-18 17:36:30 +0300

    1736

--- src/t.py
+++ src/t.py
@@ -1,3 +1,4 @@
-s = {}
-s= s.fromkeys([1,2], 't')
-print(s)
+import pyslurm
+
+job = {'job_name': 'test_0', 'partition': 'cpu_nodes', 'command': 'hostname', 'ntasks': '16', 'nodes': 5, 'output': '/tmp/test_0_%j.out'}
+pyslurm.job().submit_batch_job(job)
