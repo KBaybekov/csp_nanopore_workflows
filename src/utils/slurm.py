a94146564fc339768be8b0f220624341bed67c93
KBaybekov <91565993+KBaybekov@users.noreply.github.com>
2024-12-24 20:25:54 +0300

    2025

--- src/utils/slurm.py
+++ src/utils/slurm.py
@@ -72,7 +72,8 @@ def submit_slurm_job(command:str, working_dir:str, job_name:str, partition:str='
 
 def get_slurm_job_status(job_id:str):
     """Проверка статуса задачи через pyslurm"""
-    print(pyslurm.job().get()[job_id])
+    job_info = pyslurm.job().get().copy()
+    print(job_info.keys())
     exit()
     try:
         job_info = pyslurm.job().find_id(int(job_id))
