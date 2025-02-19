db3b3ceb6de15fc3470ba927c857c92b064fd8b4
KBaybekov <91565993+KBaybekov@users.noreply.github.com>
2025-02-19 19:20:29 +0300

    1920

--- src/human_variation.py
+++ src/human_variation.py
@@ -151,7 +151,8 @@ def generate_job_status_report(pending_jobs:dict, job_results:dict, table:pd.Dat
 
     data2print.append(''.join(table2print.to_string(index=False).replace('\t', '    ')))
     data2print = f'\n'.join(data2print)
-    os.system(f"""echo "{timestamp}\n{table.to_string().replace('\t', '    ')}" >> {log_file}""")
+    timestamp2log = timestamp + '\n'
+    os.system(f"""echo "{timestamp2log}{table.to_string().replace('\t', '    ')}" >> {log_file}""")
 
     #print job data
     os.system('clear')
